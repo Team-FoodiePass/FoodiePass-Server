687b0f41e3f150f1420ae42dfa07a3dd95bb5237
권장순 <jsoonworld@gmail.com>
2024-10-11 22:39:58 +0900

    [✨ feat/#main]: init

--- src/main/java/foodiepass/server/ServerApplication.java
+++ src/main/java/foodiepass/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package org.foodiepass.server;
+package foodiepass.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/foodiepass/server/ServerApplicationTests.java
+++ src/test/java/foodiepass/server/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package org.foodiepass.server;
+package foodiepass.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
